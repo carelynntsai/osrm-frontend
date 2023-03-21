42de6e7827a1e5b30db69f51a7588cfc3441882d
Carelynn Tsai <carelynntsai@gmail.com>
2023-03-20 21:03:54 -0400

    test

--- src/index.js
+++ src/index.js
@@ -353,6 +353,8 @@ document.getElementById('lightingCheckbox').onclick = function (e){
   displayOnePanel()
 };
 
+// test
+
 document.getElementById('sidewalksCheckbox').onclick = function (e){
   state.setSidewalks(e.target.checked);
   server = state.getServer();
